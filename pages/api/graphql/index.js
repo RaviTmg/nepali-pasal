3d2112b531751c59f7e33827ac5dacdcb1ae887f
ravitmg <tmgrawv@gmail.com>
2020-09-07 23:06:34 +0545

    added dotenv"

--- pages/api/graphql/index.js
+++ pages/api/graphql/index.js
@@ -1,20 +1,25 @@
 import { ApolloServer, makeExecutableSchema } from 'apollo-server-micro';
 import resolvers from './resolvers';
 import typeDefs from './typedefs';
-import { MongoClient } from "mongodb"
+import { MongoClient } from "mongodb";
+require('dotenv').config();
+
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
-})
-
-let db
+});
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbIdentifier = "cluster0.zfcpv.mongodb.net/nepali-pasal"
+const dbUri = `mongodb+srv://${dbUser}:${dbPass}@${dbIdentifier}?retryWrites=true&w=majority`
+let db;
 const server = new ApolloServer({
   schema,
   context: async () => {
     if (!db) {
       try {
         const dbClient = new MongoClient(
-          'mongodb+srv://ravi:ravimongo123@cluster0.zfcpv.mongodb.net/nepali-pasal?retryWrites=true&w=majority',
+          dbUri,
           {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -24,7 +29,6 @@ const server = new ApolloServer({
         if (!dbClient.isConnected()) await dbClient.connect()
         db = dbClient.db('nepali-pasal') // database name
       } catch (e) {
-        console.log('--->error while connecting with graphql context (db)', e)
       }
     }
 
